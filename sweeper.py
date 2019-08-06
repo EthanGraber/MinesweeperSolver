350cfad6ac52b8a2db6bfc2bc3ad8217a000097f
Silicate <silicatestimulus@gmail.com>
2019-08-06 11:27:47 -0400

    added board printing and basic function structure

--- sweeper.py
+++ sweeper.py
@@ -22,6 +22,31 @@ for line in opened_file:
 	line_list.extend(line)
 	solved_board.append(line_list)
 
+def reveal(rowpos, colpos):
+	pass
+def analyze():
+	pass
 
+def prettyprint(input_board):
+	separator = " | "
+	lines = []
+	for row in range(len(input_board)):
+		line = '| '
+		for col in range(len(input_board[row])):
+			line = line + input_board[row][col] + separator
+		lines.append(line)
+	for line in lines:
+		print(line)
+
+on = True
+def play():
+	move_counter = 0
+	total_boxes = length*width
+	#TODO bombs read from file
+	bombs = 21 #THIS IS THE VALUE FOR THE CURRENT MAP.TXT, ONLY HARDCODED FOR TESTING
+	while on and move_counter < 300:
+		if move_counter == 0:
+			reveal(0, 0)
+		move_counter += 1
 
 
