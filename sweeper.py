687af55979394fa9bd0330dfc2da2135856b829e
Silicate <silicatestimulus@gmail.com>
2019-08-07 11:00:09 -0400

    completed blankcheck()

--- sweeper.py
+++ sweeper.py
@@ -21,21 +21,15 @@ for line in opened_file:
 	line_list.extend(line)
 	solved_board.append(line_list)
 
-print(board)
-print(solved_board)
-
 def reveal(rowpos, colpos):
 	revealed = solved_board[rowpos][colpos]
-	print(revealed)
-	prettyprint(board)
-	print(board[rowpos][colpos])
-	board[rowpos][colpos] = revealed 
+	board[rowpos][colpos] = revealed
 	if revealed == 'B':
 		print('~~~Game Over~~~')
 
 	#Determines how many blank spaces need to be revealed
-	#elif revealed == '&':
-		#blankcheck()
+	elif revealed == '&':
+		blankcheck()
 		'''#Checks for more blank spaces to the right
 		for box in range(length):
 			col_position_holder = colpos+box+1 #to check the next box in positive direction (starts at 1 to not recount original box)
@@ -86,28 +80,46 @@ def reveal(rowpos, colpos):
 
 def blankcheck():
 	again = False
+	#For loops iterate through each row and each column within each row
 	for row in range(len(board)):
 		for col in range(len(board[row])):
-			#print("row: " + str(row))
-			#print("col : " + str(col))
-			#prettyprint(board)
 			if board[row][col] == '&':
+				#Checks column in right direction for blank
 				if col+1 < length:
+					#If blank to right, reveals it and tells loop to run again
 					if solved_board[row][col+1]=='&' and board[row][col+1]=='#':
 						board[row][col+1] = solved_board[row][col+1]
 						again = True
+					#If the program can't find a blank to the right, reveals tile
+					elif board[row][col+1] == '#':
+						board[row][col+1] = solved_board[row][col+1]
+				#Checks column in left direction for blank
 				if col-1 > -1:
+					#If blank to left, reveals it and tells loop to run again
 					if solved_board[row][col-1]=='&' and board[row][col-1]=='#':
 						board[row][col-1] = solved_board[row][col-1]
 						again = True
+					#If the program can't find a blank to the left, reveals tile
+					elif board[row][col-1] == '#':
+						board[row][col-1] = solved_board[row][col-1]
+				#Checks row above for blank
 				if row+1 < width:
+					#If blank above, reveals it and tells loop to run again
 					if solved_board[row+1][col]=='&' and board[row+1][col]=='#':
 						board[row+1][col] = solved_board[row+1][col]
 						again = True
+					#If the program can't find a blank above, reveals tile
+					elif board[row+1][col] == '#':
+						board[row+1][col] = solved_board[row+1][col]
+				#Checks row below for blank
 				if row-1 > -1:
+					#If blank below, reveals it and tells loop to run again
 					if solved_board[row-1][col]=='&' and board[row-1][col]=='#':
 						board[row-1][col] = solved_board[row-1][col]
 						again = True
+					#If the program can't find a blank below, reveals tile
+					elif board[row-1][col] == '#':
+						board[row-1][col] = solved_board[row-1][col]
 	if again:
 		blankcheck()
 def analyze():
@@ -133,7 +145,7 @@ def play():
 	bombs = 21 #THIS IS THE VALUE FOR THE CURRENT MAP.TXT, ONLY HARDCODED FOR TESTING
 	while on and move_counter < 300:
 		if move_counter == 0:
-			reveal(0, 3)
+			reveal(0, 0)
 			prettyprint(board)
 			on = False
 		move_counter += 1
